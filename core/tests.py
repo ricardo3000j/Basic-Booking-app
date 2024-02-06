cf52a48ba87c849c7189f101ab7614510ca01f2a
ricardo3000j <ricardojarquin3000j@gmail.com>
2024-02-06 08:45:56 -0600

    test cases

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from core.serializers import BookingSerializer
+from .models import Property, Booking, PricingRule
+
+
+class BookingTestCase(TestCase):
+    def setUp(self):
+        self.property = Property.objects.create(base_price=10)
+
+    def test_case_1(self):
+        PricingRule.objects.create(
+            property=self.property, min_stay_length=7, price_modifier=-10
+        )
+        serializer = BookingSerializer(
+            data={
+                "property": self.property.id,
+                "date_start": date(2022, 1, 1),
+                "date_end": date(2022, 1, 10),
+            }
+        )
+        if serializer.is_valid():
+            booking = serializer.save()
+            self.assertAlmostEqual(booking.final_price, 90)
+
+    def test_case_2(self):
+        PricingRule.objects.create(
+            property=self.property, min_stay_length=7, price_modifier=-10
+        )
+        PricingRule.objects.create(
+            property=self.property, min_stay_length=30, price_modifier=-20
+        )
+        serializer = BookingSerializer(
+            data={
+                "property": self.property.id,
+                "date_start": date(2022, 1, 1),
+                "date_end": date(2022, 1, 10),
+            }
+        )
+        if serializer.is_valid():
+            booking = serializer.save()
+            self.assertAlmostEqual(booking.final_price, 90)
+
+    def test_case_3(self):
+        PricingRule.objects.create(
+            property=self.property, min_stay_length=7, price_modifier=-10
+        )
+        PricingRule.objects.create(
+            property=self.property, specific_day=date(2022, 1, 4), fixed_price=20
+        )
+        serializer = BookingSerializer(
+            data={
+                "property": self.property.id,
+                "date_start": date(2022, 1, 1),
+                "date_end": date(2022, 1, 10),
+            }
+        )
+        if serializer.is_valid():
+            booking = serializer.save()
+            self.assertAlmostEqual(booking.final_price, 101)
