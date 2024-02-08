5c1cb72ef7fda23cb64975b9bbfa27f31c508932
ricardo3000j <ricardojarquin3000j@gmail.com>
2024-02-07 23:09:57 -0600

    add allowed host and multi schema support for Swagger

--- reservations/urls.py
+++ reservations/urls.py
@@ -20,6 +20,14 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.http import HttpResponseRedirect
+from drf_yasg.generators import OpenAPISchemaGenerator
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
 
 
 def redirect_to_docs(request):
@@ -34,6 +42,7 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
 )
 
 urlpatterns = [
